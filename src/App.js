52ea33e593c27ad7314180ec315bfc9465872a42
crashdaddy <kennethscoggins@mail.com>
2022-09-07 17:12:06 -0500

    initial framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import { Component } from 'react';
+import Tile from ".//Components/Tile.js";
+
+  window.addEventListener('keydown', (event) => {
+    // ...
+  });
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  this.state = {direction: " Press an arrow key",
+  board:[
+    [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
+    [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
+    [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
+    [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"R",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
+    [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}],
+    [{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"},{letter:"",status:"white"}]
+  ]
+  };
+  }
+
+handleKey = (e) => {
+  this.setState({
+    direction:e.code
+  })
 }
 
-export default App;
+handleKeyUp =() => {
+  this.setState({
+    direction:"Press an arrow key"
+  })
+}
+
+componentDidMount = () => {
+ window.addEventListener("keydown",this.handleKey)
+ window.addEventListener("keyup",this.handleKeyUp)
+} 
+  render () {
+
+    return(
+    <div className="App">
+      <header className="App-header">
+      <div style={{width:'220px'}}>
+              {this.state.board.map((board,idx)=>{
+              return (
+              <div key = {idx}>
+                  {board.map((row,idx)=>{
+                  return(
+                  <div><Tile letter={row.letter} color={row.status} key={idx}/></div>
+                  )
+                  })
+                  }  
+              </div>
+              )
+              })}
+        </div>
+        <p>
+          {this.state.direction}
+        </p>
+       
+      </header>
+    </div>
+    );
+}
+
+}
+
+export default App
